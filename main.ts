a2fafcd3186d5c89f089a32e647894a296dd177b
Sh2009hi <122560271+Sh2009hi@users.noreply.github.com>
2023-01-13 13:13:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.forever(function () {
+	
+})
